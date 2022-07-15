ffd51086deeabc17174514fe769f4f6228957136
LucasDev <lucas.mendes@fitbank.com.br>
2022-07-15 14:54:46 -0300

    fix: Stopwatch done.

--- index.js
+++ index.js
@@ -5,62 +5,39 @@ let bt_z = document.getElementsByClassName("bt-zerar")
 let seg = 0
 let min = 0
 let hora = 0
+let interval
+
+function doisDigitos(n){
+    if(n<10){
+        return `0${n}`
+    }else{
+        return n
+    }
+}
 
 function time(){
-    
+    seg++
+    if(seg==60){
+        min++
+        seg=0
+        if(min==60){
+            hora++
+            min=0
+        }
+    }
+
+    ch1.innerText = `${doisDigitos(hora)}:${doisDigitos(min)}:${doisDigitos(seg)}`
+
 }
 
 function iniciar(){
-    setInterval(time, 1)
+   interval = setInterval(time, 1)
 }
 
 function zerar(){
+    clearInterval(interval)
+    seg=0
+    min=0
+    hora=0
     ch1.innerText = `00:00:00`
-    numero = 0
-    document.location.reload()//Recarrega a página que daí o cronômetro para
 }
-
-
-
-
-// if(hora<10 && min<10 && seg<10){
-//     ch1.innerText = `0${hora}:0${min}:0${seg}`
-//     seg++
-// }else if(hora<10 && min<10 && seg<60){
-//     ch1.innerText = `0${hora}:0${min}:${seg}`
-//     seg++
-// }else if(seg>=60 && seg<=3600){
-
-//    min += 1
-//    seg = seg%60
-
-//    if(hora<10 && min<10 && seg<10){
-//     ch1.innerText =`0${hora}:0${min}:0${seg}`
-//     seg++
-//     }else if(hora<10 && min<10 && seg>=10){
-//         ch1.innerText =`0${hora}:0${min}:${seg}`
-//         seg++
-//     }else if(hora<10 && min>=10 && seg>=10){
-//         ch1.innerText =`0${hora}:${min}:${seg}`
-//         seg++
-//     }else if(hora<10 && min>=10 && seg<10){
-//         ch1.innerText =`0${hora}:${min}:0${seg}`
-//         seg++
-//     }else if(hora>=10 && min<10 && seg<10){
-//         ch1.innerText =`${hora}:0${min}:0${seg}`
-//         seg++
-//     }else if(hora>=10 && min<10 && seg>=10){
-//         ch1.innerText =`0${hora}:0${min}:${seg}`
-//         seg++
-//     }else if(hora>=10 && min>=10 && seg<10){
-//         ch1.innerText =`${hora}:${min}:0${seg}`
-//         seg++
-//     }else{
-//         ch1.innerText =`${hora}:${min}:${seg}`
-//         seg++
-//     }
-// }else{
-//     hora = hora + 1
-//     min = seg%60
-//     seg = seg%60
-// }
